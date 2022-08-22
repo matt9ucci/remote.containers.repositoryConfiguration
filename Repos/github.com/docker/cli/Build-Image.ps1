568c27291d387903160f8f75e5ac6eb23b277a7e
Matt Gucci <matt9ucci@gmail.com>
2022-08-22 21:14:10 +0900

    Build image: github.com/docker/cli

---
+++ Repos/github.com/docker/cli/Build-Image.ps1
@@ -0,0 +1,4 @@
+git clone https://github.com/docker/cli --depth 1 $PSScriptRoot\cli
+Push-Location $PSScriptRoot\cli
+docker build -t docker-cli-dev:local -f $PSScriptRoot\cli\dockerfiles\Dockerfile.dev .
+Pop-Location
